a1b2de88b575e30ada9f13f8d9e8ceac93339182
Chemweno Collins <collinskiprop@gmail.com>
2025-12-14 23:30:54 +0300

    Complete scrapper of the websites SimpleFlying and SpaceNews

--- Main.py
+++ Main.py
@@ -18,13 +18,14 @@ html_content=response.text
 soup=BeautifulSoup(html_content, "html.parser")
 
 # Find all article titles on the page
-article=soup.find_all("a", class_="display-card-title")
+article=soup.find_all("h3", class_="display-card-title")
 if not article:
     articles=soup.find_all("article")
 
 news_data=[]
 
-for item in articles:
+# Extract headlines and links from the articles
+for item in article:
     link_element=None
     if item.name=='a':
         link_element=item
@@ -40,17 +41,42 @@ for item in articles:
 
         if headline:
             news_data.append({
+                "source": "simpleflying",
                 "Headline": headline,
                 "Link": link
             })
 
-df=pd.DataFrame(news_data)
-
-print("-- Latest News Articles --")
-
-if not df.empty:
-    print(df(10))
-else:
-    print("No articles found.")
 
 
+# Now scrape SpaceNews
+url_sn="https://spacenews.com/"
+response_sn=requests.get(url_sn, headers=HEADER)
+soup_sn=BeautifulSoup(response_sn.text, "html.parser")
+sn_data=[]
+articles_sn=soup_sn.find_all(["h3","h2"], class_=lambda x: x and "title" in x)
+for item in articles_sn:
+    link_tag=item.find("a")
+    if link_tag:
+        headline=link_tag.get_text(strip=True)
+        link=link_tag['href']
+
+        if headline:
+            sn_data.append({
+                "source": "spacenews",
+                "Headline": headline,
+                "Link": link
+            })
+
+
+
+
+# Combine data from both sources
+df1=pd.DataFrame(news_data)
+df2=pd.DataFrame(sn_data)
+
+combined_df=pd.concat([df1, df2], ignore_index=True)
+combined_df.to_csv("aerospace_news.csv", index=False)
+
+print(f"Scraped {len(combined_df)} articles and saved to aerospace_news.csv")
+print("saved to aerospace_news.csv")
+print(combined_df.head(20))
--- test.py
+++ test.py
@@ -17,4 +17,15 @@ html_content=response.text
 #Create a BeautifulSoup object to parse the HTML content
 soup=BeautifulSoup(html_content, "html.parser")
 
-print(soup.prettify())
+# Find all article titles on the page
+article=soup.find_all("h3", class_="display-card-title")
+
+#print(article)
+
+
+url_sn="https://spacenews.com/"
+response_sn=requests.get(url_sn, headers=HEADER)
+soup_sn=BeautifulSoup(response_sn.text, "html.parser")
+sn_data=[]
+articles_sn=soup_sn.find_all(["h3","h2"], class_=lambda x: x and "title" in x)
+print(articles_sn)
